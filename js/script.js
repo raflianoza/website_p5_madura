8e40d7fcca9b6c819f42f30d37c9c8eb5ae00f58
kelaskazeters <dev.raflianoza@gmail.com>
2024-09-30 20:07:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// some secret massage :D
+console.log("Hei kamu! Sedang apa disana? :)")
+console.log("Iseng banget ngecek console, xixixixi.")
+console.log("Btw, made with love by Rafliano Ziyad Adzani.")
+console.log("Supported by : Denivo, Jihan, Mirza, Pascal, Soraya, Zaki, and Zaidan.")
+
+// ==================INDEX========================
